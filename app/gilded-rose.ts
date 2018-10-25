007332a95dfe085eefe68dc59901c00b8166096b
Nikola Horvat <nikolahr@gmail.com>
2018-10-25 21:03:31 +0200

    Update test cases for Sulfuras

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,7 +19,9 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].quality > 50) { this.items[i].quality = 50; }
+            if (this.items[i].quality > 50) {
+                this.items[i].quality = 50;
+            }
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -27,6 +29,8 @@ export class GildedRose {
                           if (this.items[i].name == 'Conjured') {
                             this.items[i].quality = this.items[i].quality - 1;
                           }
+                    } else {
+                        this.items[i].quality = 80;
                     }
                 }
             } else {
@@ -56,6 +60,8 @@ export class GildedRose {
                                   if (this.items[i].name == 'Conjured') {
                                     this.items[i].quality = this.items[i].quality - 1;
                                   }
+                            } else {
+                              this.items[i].quality = 80;
                             }
                         }
                     } else {
