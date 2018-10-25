5a5327701b0d18490872e0dc52b3aa0272da2044
Nikola Horvat <nikolahr@gmail.com>
2018-10-25 19:10:28 +0200

    Test degrading of goods quality

--- test/guilded-rose.spec.ts
+++ test/guilded-rose.spec.ts
@@ -2,11 +2,17 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+  describe('goods quality', function () {
+    const gildedRose = new GildedRose([
+      new Item('Milk', 5, 10),
+      new Item('Milk', 0, 5)
+    ]);
+    const items = gildedRose.updateQuality();
+    it('should degrade as they approach the sell date', function() {
+      expect(items[0].quality).to.equal(9);
     });
-
+    it('should degrade twice as fast once the sell by date has passed', function() {
+      expect(items[1].quality).to.equal(3);
+    });
+  });
 });
