022f4b1a968a85abcf9d3a3c94b40b00cbe6d99d
Nikola Horvat <nikolahr@gmail.com>
2018-10-25 20:31:01 +0200

    Remove unnecessary IF statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,22 +23,20 @@ export class GildedRose {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
+                        this.items[i].quality = this.items[i].quality - 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
+                this.items[i].quality = this.items[i].quality + 1;
+                if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (this.items[i].sellIn < 11) {
+                        if (this.items[i].quality < 50) {
+                            this.items[i].quality = this.items[i].quality + 1;
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
+                    }
+                    if (this.items[i].sellIn < 6) {
+                        if (this.items[i].quality < 50) {
+                            this.items[i].quality = this.items[i].quality + 1;
                         }
                     }
                 }
@@ -51,20 +49,17 @@ export class GildedRose {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
+                                this.items[i].quality = this.items[i].quality - 1;
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
+                    this.items[i].quality = this.items[i].quality + 1;
                 }
             }
         }
-
         return this.items;
     }
 }
